c8a2aaa057ce4e6f55ce7648491010cfac7e589e
Rolf Michelsen <rolf@rolfmichelsen.com>
2019-10-29 23:17:22 +0100

    Update assembly information

---
+++ DragonDosTools/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DragonDos")]
+[assembly: AssemblyDescription("Access virtual DragonDos filesystems.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rolf Michelsen")]
+[assembly: AssemblyProduct("DragonTools")]
+[assembly: AssemblyCopyright("Copyright ©  2011-2019 Rolf Michelsen")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0634aca3-3fac-4699-9df3-db396ae4ad9b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
